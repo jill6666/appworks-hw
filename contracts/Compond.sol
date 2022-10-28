8a35a09d5340c521b3afdf0986decd8f49f1c598
jillchang <jill.chang@jkos.com>
2022-10-28 14:22:08 +0800

    feat: test for compound

--- contracts/Compond.sol
+++ contracts/Compond.sol
@@ -3,27 +3,23 @@
 pragma solidity ^0.8.16;
 
 // Import CErc20 and Comptroller
+import 'compound-protocol/contracts/CErc20.sol';
 import 'compound-protocol/contracts/CErc20Delegator.sol';
 import 'compound-protocol/contracts/CErc20Delegate.sol';
 import 'compound-protocol/contracts/Comptroller.sol';
 import 'compound-protocol/contracts/Unitroller.sol';
-import 'compound-protocol/contracts/InterestRateModel.sol';
+import 'compound-protocol/contracts/WhitePaperInterestRateModel.sol';
 import 'compound-protocol/contracts/SimplePriceOracle.sol';
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+import 'hardhat/console.sol';
 
 /**
  * 
  */
 contract TestToken is ERC20 {
-	constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-		uint256 n = 100000000;
-		_mint(msg.sender, n * 10**uint256(decimals()));
-	}
-
-	function mint(address account) public virtual {
-		uint256 n = 10000;
-		_mint(account, n * 10**uint256(decimals()));
+	constructor(uint256 supply, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
 	}
 }
 
